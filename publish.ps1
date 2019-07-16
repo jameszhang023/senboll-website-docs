d7182c1d2167f70fc6a4c5f43397c3745635c446
James Zhang <1329485053@qq.com>
2019-07-16 10:56:14 +0800

    update

--- publish.ps1
+++ publish.ps1
@@ -1,8 +1,3 @@
 git add .
-git commit -m update
+git commit -m "update"
 git push remote origin master
-Copy-Item C:\Users\zhang\Documents\Control4\PublishedDrivers\ParadoxDriver.c4z  D:\Devlop\WebSite\hugo-create-website\content\node2\paradox-to-control4-driver\_index.files\
-Copy-Item D:\Devlop\Control4\ParadoxDriver\driver.xml  D:\Devlop\WebSite\hugo-create-website\content\node2\paradox-to-control4-driver\_index.files\
-hugo 
-Remove-Item \\192.168.8.4\webroot\help\* -Recurse
-Copy-Item D:\Devlop\WebSite\hugo-create-website\public\*  \\192.168.8.4\webroot\help\ -recurse
