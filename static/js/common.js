75250fd5830ee18d2ed5e6ba30108c619a07c65f
James Zhang <1329485053@qq.com>
2019-08-09 11:21:42 +0800

    update

--- static/js/common.js
+++ static/js/common.js
@@ -1,12 +1,12 @@
 // mobile menu icon auto switch
 function mobilemenubutton() {
   // Find any menu items that are open  
-  var ariaExpanded = document.getElementById('mobilemenubutton').getAttribute("aria-expanded");
+  var ariaExpanded = document.getElementById('mobile-menu-button').getAttribute("aria-expanded");
   if(ariaExpanded == "false") {
-    document.getElementById('mobilemenubutton').setAttribute("aria-expanded", "true");
-    document.getElementById('mobilemenu').setAttribute("aria-expanded", "true");
+    document.getElementById('mobile-menu-button').setAttribute("aria-expanded", "true");
+    document.getElementById('mobile-menu').setAttribute("aria-expanded", "true");
   }else{
-    document.getElementById('mobilemenubutton').setAttribute("aria-expanded", "false");
-    document.getElementById('mobilemenu').setAttribute("aria-expanded", "false");
+    document.getElementById('mobile-menu-button').setAttribute("aria-expanded", "false");
+    document.getElementById('mobile-menu').setAttribute("aria-expanded", "false");
   }
 }
