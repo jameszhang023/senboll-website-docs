b5f22ae07bb32b2545c533f374973cbf538e7e37
James Zhang <1329485053@qq.com>
2019-08-20 17:37:09 +0800

    update

--- static/js/common.js
+++ static/js/common.js
@@ -90,6 +90,70 @@ window.onload = function(){
 };
 
 
-$(function(){
-  $('img').zoomify();
-});
+window.onload = function(){
+    var $ = {};
+    /**
+     * get multiple elements
+     * @public
+     */
+    $.all = function(selector, contextElement) {
+      var nodeList,
+        list = [];
+      if (contextElement) {
+        nodeList = contextElement.querySelectorAll(selector);
+      } else {
+        nodeList = document.querySelectorAll(selector);
+      }
+      if (nodeList && nodeList.length > 0) {
+        list = Array.prototype.slice.call(nodeList);
+      }
+      return list;
+    }
+
+    /**
+     * delegate an event to a parent element
+     * @public
+     * @param  array     $el        parent element
+     * @param  string    eventType  name of the event
+     * @param  string    selector   target's selector
+     * @param  function  fn
+     */
+    $.delegate = function($el, eventType, selector, fn) {
+      if (!$el) { return; }
+      $el.addEventListener(eventType, function(e) {
+        var targets = $.all(selector, $el);
+        if (!targets) {
+          return;
+        }
+        // findTarget:
+        for (var i=0; i<targets.length; i++) {
+          var $node = e.target;
+          while ($node) {
+            if ($node == targets[i]) {
+              fn.call($node, e);
+              break; //findTarget;
+            }
+            $node = $node.parentNode;
+            if ($node == $el) {
+              break;
+            }
+          }
+        }
+      }, false);
+    };
+
+    var urls = [];
+    var imgs = $.all('img',$.all('#prose')[0]);
+    imgs.forEach(function(v,i){
+        urls.push(v.src);
+    })
+    
+    $.delegate(document.querySelector('#prose'), 'click','img',function(){
+        var current = this.src;
+        var obj = {
+            urls : urls,
+            current : current
+        };
+        previewImage.start(obj);
+    });
+  };
