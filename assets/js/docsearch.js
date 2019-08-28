b4ebe4175c5e934e50d41fe19e9bec90fb86b156
James Zhang <1329485053@qq.com>
2019-08-28 11:41:47 +0800

    update

--- assets/js/docsearch.js
+++ assets/js/docsearch.js
@@ -1,7 +1,7 @@
 var docsearch = require('docsearch.js/dist/cdn/docsearch.js');
 docsearch({
   apiKey: '58d6e22d9231268da7546d132812c1f1',
-  indexName: 'support-senboll',
+  indexName: 'hugodocs',
   inputSelector: '#search-input',
   debug: true // Set debug to true if you want to inspect the dropdown
 });
