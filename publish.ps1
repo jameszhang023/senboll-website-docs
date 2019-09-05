a0e9d11b9d40151c68784e00d75ca65c75fc69e6
James Zhang <1329485053@qq.com>
2019-09-05 17:02:54 +0800

    update

--- publish.ps1
+++ publish.ps1
@@ -6,6 +6,6 @@ git push origin master
 Copy-Item C:\Users\zhang\Documents\Control4\PublishedDrivers\ParadoxDriver.c4z  content\zh\node2\paradox-to-control4-driver\_index.files\
 Copy-Item D:\Devlop\Control4\ParadoxDriver\driver.xml  content\zh\node2\paradox-to-control4-driver\_index.files\
 hugo 
-npm run algolia
 Remove-Item \\192.168.8.4\webroot\help\* -Recurse
 Copy-Item D:\Devlop\WebSite\senboll-support-website\public\*  \\192.168.8.4\webroot\help\ -recurse
+npm run algolia
