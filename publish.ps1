485b3c2eede9c9fc1578cd142f41f9d2c1fe9b83
James Zhang <1329485053@qq.com>
2019-11-06 15:59:07 +0800

    update

--- publish.ps1
+++ publish.ps1
@@ -7,5 +7,5 @@ Copy-Item C:\Users\zhang\Documents\Control4\PublishedDrivers\ParadoxDriver.c4z  
 Copy-Item D:\Devlop\Control4\ParadoxDriver\driver.xml  content\zh\node2\paradox-to-control4-driver\_index.files\
 hugo 
 Remove-Item \\192.168.8.4\webroot\help\* -Recurse
-Copy-Item D:\Devlop\WebSite\senboll-support-website\public\*  \\192.168.8.4\webroot\help\ -recurse
+Copy-Item D:\Devlop\WebSite\senboll-support-website\public\*  \\192.168.8.4\webroot$\help\ -recurse
 npm run algolia
