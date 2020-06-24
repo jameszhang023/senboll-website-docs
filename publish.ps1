e2d7488cdb1b81866aec570f65145028370b4c83
james <1329485053@qq.com>
2020-06-24 23:38:22 +0800

    update

--- publish.ps1
+++ publish.ps1
@@ -4,8 +4,8 @@ git add .
 git commit -m "update"
 git push origin master
 Copy-Item C:\Users\zhang\Documents\Control4\PublishedDrivers\ParadoxDriver.c4z  content\zh\node2\paradox-to-control4-driver\_index.files\
-Copy-Item D:\Devlop\Control4\ParadoxDriver\driver.xml  content\zh\node2\paradox-to-control4-driver\_index.files\
+Copy-Item D:\develop\Control4\ParadoxDriver\driver.xml  content\zh\node2\paradox-to-control4-driver\_index.files\
 hugo 
 Remove-Item \\192.168.8.8\webroot$\help\* -Recurse
-Copy-Item D:\Devlop\WebSite\senboll-support-website\public\*  \\192.168.8.8\webroot$\help\ -recurse
+Copy-Item D:\develop\WebSite\senboll-support-website\public\*  \\192.168.8.8\webroot$\help\ -recurse
 npm run algolia
