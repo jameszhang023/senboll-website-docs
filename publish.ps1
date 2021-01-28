23d16cda0fbd1a58127744e4c2d9fca30dfb0356
james <1329485053@qq.com>
2021-01-28 16:12:46 +0800

    update

--- publish.ps1
+++ publish.ps1
@@ -6,6 +6,6 @@ git push origin master
 Copy-Item C:\Users\zhang\Documents\Control4\PublishedDrivers\ParadoxDriver.c4z  content\zh\node2\paradox-to-control4-driver\_index.files\
 Copy-Item D:\develop\Control4\ParadoxDriver\driver.xml  content\zh\node2\paradox-to-control4-driver\_index.files\
 hugo 
-Remove-Item \\192.168.8.8\webroot$\help\* -Recurse
-Copy-Item public/*  \\192.168.8.8\webroot$\help\ -recurse
+Remove-Item \\192.168.8.8\webroot$\docs\* -Recurse
+Copy-Item public/*  \\192.168.8.8\webroot$\docs\ -recurse
 npm run algolia
